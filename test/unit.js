31cc0325dbaacde312f04fd2bf9303c28c7cf8ce
David MARTIN <david@dmidz.com>
2017-10-07 03:45:22 +0200

    main commit

--- test/unit.js
+++ test/unit.js
@@ -1,40 +1,21 @@
 
-const Path = require('path')
+const Lab = require('lab')
+	, lab = exports.lab = Lab.script()
+	// , Path = require('path')
 	// , dutils = require( 'dmidz-utils')
 	// , Promise = require('bluebird')
-	, App = require( '../lib')
-	, Lab = require('lab')
-	, lab = exports.lab = Lab.script()
-	, plugin_route_prefix = '/myprefix'
 	;
 
-lab.experiment('dmidz-app', function(){
+lab.experiment('dmidz-hapi-content-negociatino', function(){
 
-	lab.before(function( done ){
-
-		app.initialize().then( function(){
-			done();
-		});
-
-	});
+	// lab.before(function( done ){
+	//
+	// });
 
 	//_________ tests
-	lab.test('initialize with no errors, having routes.', function( done ){
-		const routes = app.server.getAllRoutes();
-		Lab.expect( routes ).to.be.an.array();
-		Lab.expect( routes ).to.have.length( 2 );
+	lab.test('TODO test', function( done ){
 		done();
 	});
 
-	lab.test('plugin route with prefix', function(){
-		return app.server.inject( {
-			url : plugin_route_prefix+'/hello'
-			, method : 'get'
-		} ).then( function( res ){
-			Lab.expect( res.statusCode ).to.equal( 200 );
-			Lab.expect( res.result ).to.be.an.object();
-			Lab.expect( res.result.title ).to.be.equal('homepage');
-		});
-	});
 });
 
