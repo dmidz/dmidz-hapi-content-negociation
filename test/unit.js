c20acdff659cc3a5619f9a99549affb8e97c2ec3
David MARTIN <david@dmidz.com>
2017-10-16 23:27:55 +0200

    unit tests

--- test/unit.js
+++ test/unit.js
@@ -1,16 +1,39 @@
 
 const Lab = require('lab')
 	, lab = exports.lab = Lab.script()
+	, Hapi = require('hapi')
 	// , Path = require('path')
 	// , dutils = require( 'dmidz-utils')
 	// , Promise = require('bluebird')
 	;
 
-lab.experiment('dmidz-hapi-content-negociatino', function(){
+lab.experiment('dmidz-hapi-content-negociation', function(){
 
-	// lab.before(function( done ){
-	//
-	// });
+	//__ server
+	let server = new Hapi.Server();
+
+
+	lab.before(function( done ){
+		//__ connections
+		server.connection( {
+			host: 'localhost'
+			, port : process.env.port || 3000
+		} );
+
+		server.route( [{//_ home
+			method: 'GET'
+			, path: '/'
+			, config : {
+				handler: function (request, reply) {
+					return reply({ title: 'homepage', content:'Hello !' });
+				}
+			}
+		}] );
+
+		server.initialize().then(function(){
+			done();
+		});
+	});
 
 	//_________ tests
 	lab.test('TODO test', function( done ){
